d82e231f92f1716b3470c79d1b129d369ac4b1e5
Kirill Dovganyuk <kiridovg@gmail.com>
2021-06-11 14:29:38 +0300

    Update files

---
+++ public/views/about.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+        <div class="my-3 p-3">
+            <h2 class="display-5">О товаре</h2>
+            <p class="lead">Тут вы узнаете всю информацию про ваш товар.</p>
+        </div>
+        <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+    </div>
+</div>
