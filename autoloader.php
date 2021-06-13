d8b1df1c1bc0ee4892ecdd09b22d4f2568f41178
Kirill Dovganyuk <kiridovg@gmail.com>
2021-06-13 19:29:48 +0300

    Task* Plus Autoloader

--- autoloader.php
+++ autoloader.php
@@ -2,13 +2,17 @@
 
 class Autoloader
 {
+    public $rules;
     public function __construct(array $rules = [])
     {
-
+        $this->rules = $rules;
     }
 
     public function load($classname)
     {
+        foreach($this->rules as $key => $value){
+            $classname = str_replace($key, $value, $classname);
+        }
         $dir = explode('\\', $classname);
         $dir = implode('/', $dir) . '.php';
         if (is_file($dir)) {
--- public/index.php
+++ public/index.php
@@ -3,9 +3,11 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 
 include_once '../autoloader.php';
-$autoloader = new Autoloader();
-$autoloader->load('..\\Framework\\Render\\RenderInterface');
-$autoloader->load('..\\Framework\\Render\\Render');
+$autoloader = new Autoloader([
+    'App\\' => 'Framework/',
+]);
+$autoloader->load('..\\App\\Render\\RenderInterface');
+$autoloader->load('..\\App\\Render\\Render');
 
 $goods = json_decode(file_get_contents('../db.txt'), true);
 $templates = 'views/productlist';
