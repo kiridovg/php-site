83b59101dbb9520e9e10e603cbe1f7e88cf9e07f
Kirill Dovganyuk <kiridovg@gmail.com>
2021-06-16 16:36:15 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,19 +4,13 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-use App\Core\View;
+use Framework\Router\Router;
 
 include_once '../vendor/autoload.php';
 
 $goods = json_decode(file_get_contents('../db.txt'), true);
-$templates = '../App/View/Templates/productlist.php';
-$layout = 'layout.php';
-$params = [
-    "id" => "123 ",
-    "name" => "Kirill",
-    "title" => "Список товаров",
-    "goods" => $goods,
-];
 
-$render = New View();
-$render->render($templates, $params,  $layout);
+$router = New Router();
+
+$router->get('/', [App\Controller\Product::class, 'getProductList']);
+$router->run();
