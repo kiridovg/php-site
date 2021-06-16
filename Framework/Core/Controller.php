4b13d4f6b53b2d3ec2496147396e18c5b76c5700
Kirill Dovganyuk <kiridovg@gmail.com>
2021-06-16 16:35:52 +0300

    Update MVC

--- Framework/Core/Controller.php
+++ Framework/Core/Controller.php
@@ -2,14 +2,18 @@
 
 namespace Framework\Core;
 
+use Framework\Database\Database;
+
 class Controller{
 
+    public static Database $db;
     public View $view;
     public Model $model;
 
     function __construct(){
         $this->view = new View();
         $this->model = new Model();
+        $this::$db = new Database();
     }
     function action(){
 
