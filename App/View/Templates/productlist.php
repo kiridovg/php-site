fd3c03771a2f18bd87598ca6fde7f61ab4627734
Kirill Dovganyuk <kiridovg@gmail.com>
2021-06-19 17:42:40 +0300

    Add style

--- App/View/Templates/productlist.php
+++ App/View/Templates/productlist.php
@@ -1,18 +1,61 @@
+<style>
+
+    .imgn{
+        height: 200px;
+        width: auto;
+
+    }
+    .price{
+        height: auto;
+        margin-top: auto;
+    }
+    .card {
+        border: none;
+        border-radius: 10px
+    }
+
+    .c-details span {
+        font-weight: 300;
+        font-size: 13px;
+    }
+
+    .badge span {
+        background-color: #fffbec;
+        width: 80px;
+        height: 35px;
+        font-size: 25px;
+        padding-bottom: 3px;
+        border-radius: 5px;
+        display: flex;
+        color: #000;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .name{
+        justify-content: center;
+        align-items: center;
+    }
+
+    .progress div {
+        background-color: red;
+    }
+
+    .text1 {
+        font-size: 25px;
+        font-weight: 600;
+    }
+
+</style>
 <div class="container">
     <div class="me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
         <div class="my-3 p-3">
             <h2 class="display-5">Список товаров</h2>
-            <?php
-            foreach ($params as $good){
-                echo "<p class='id'>Номер: ${good['Id']}</p></br>";
-                echo "<p class='name'>Название: ${good['Name']}</p></br>";
-                echo "<p class='description'>Цена: ${good['Price']}</p></br>";
-                echo "<p class='description'>Количество: ${good['Count']}</p></br>";
+            <div class="container mt-5">
+                <div id="product-card">
 
-            }
-            ?>
-
+                </div>
+            </div>
         </div>
-        <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
     </div>
 </div>
