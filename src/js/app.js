35cee554172d3f72725eaae857887980921f76f0
Kirill Dovganyuk <kiridovg@gmail.com>
2021-06-22 12:52:44 +0300

    Add sort

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,12 @@ window.Vue = require('vue')
 Vue.component('productlist', require('./components/Productlist.vue').default)
 Vue.component('product', require('./components/Product.vue').default)
 Vue.component('productid', require('./components/Productid.vue').default)
+Vue.component('productsort', require('./components/ProductSort.vue').default)
+
 
 
 const app = new Vue({
   el: '#app',
 })
+
+const eventBus = new Vue()
